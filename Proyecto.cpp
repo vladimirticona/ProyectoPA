96e8e6d9ea3947b7c145ae91170376b83ee777b9
VladimirTicona <vladimirticona369@gmail.com>
2024-01-27 19:32:33 -0500

    Implementando las opciones

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -22,7 +22,22 @@ void Tarea::mostrarTarea(){
 	cout<<"Fecha de entrega: "<<dia<<" / "<<mes<<" / 2024"<<endl;
 }
 
+//---------------------------------------
+
 int main(){
+	int opcion;
+	do{
+		cout<<"----------BIENVENIDO AL ADMINISTRADOR DE TAREAS----------"<<endl;
+		cout<<endl;
+		cout<<"1) Ver tareas pendientes"<<endl;
+		cout<<"2) Insertar nueva tarea"<<endl;
+		cout<<"3) Eliminar tarea"<<endl;
+		cout<<"4) Anotar fechas o datos importantes"<<endl;
+		cout<<"0) Salir"<<endl<<endl;
+		cout<<"Elije una opcion: ";
+		cin>>opcion;	
+	}while(opcion!=0);
+	
 	
 	return 0;
 }
