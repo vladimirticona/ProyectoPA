0b4441ca9e1e444d51905b5f018b27a0453d266e
VladimirTicona <vladimirticona369@gmail.com>
2024-01-29 23:24:54 -0500

    Proyecto terminado

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -70,8 +70,29 @@ void Informe::mostrarTarea(){
 
 //------------------------
 
+class Examen : public Tarea{
+	private:
+		int cantidad;
+	public:
+		Examen(string,int,int,int);
+		void mostrarTarea();
+};
 
+Examen::Examen(string _curso,int _dia,int _mes,int _cantidad) : Tarea(_curso,_dia,_mes){
+	cantidad=_cantidad;
+}
+
+void Examen::mostrarTarea(){
+	Tarea::mostrarTarea();
+	cout<<"Cantidad de preguntas: "<<cantidad<<endl;
+}
+
+
+
+
+//------------------------
 int main(){
+	Examen *p[100];
 	Informe *z[100];
 	Exposicion *y[100];
 	Tarea *x[100];
@@ -79,21 +100,28 @@ int main(){
 	int cont=0;
 	int conta=0;
 	int contad=0;
+	int contado=0;
 	
 	do{
 		cout<<"----------BIENVENIDO AL ADMINISTRADOR DE TAREAS----------"<<endl;
 		cout<<endl;
 		cout<<"1) Ver tareas pendientes"<<endl;
 		cout<<"2) Insertar nueva tarea"<<endl;
-		cout<<"3) Que hago primero"<<endl;
+		cout<<"3) Que tarea hago primero"<<endl;
 		cout<<"4) Eliminar tarea"<<endl<<endl;
 		cout<<"5) Ver exposiciones pendientes"<<endl;
 		cout<<"6) Insertar nueva exposicion"<<endl;
-		cout<<"7) Eliminar exposicion"<<endl<<endl;
-		cout<<"8) Ver informes pendientes"<<endl;
-		cout<<"9) Insertar nuevo informe"<<endl;
-		cout<<"10) Eliminar informe"<<endl<<endl;
-		cout<<"11) Anotar fechas o datos importantes"<<endl;
+		cout<<"7) Que exposicion hago primero"<<endl;
+		cout<<"8) Eliminar exposicion"<<endl<<endl;
+		cout<<"9) Ver informes pendientes"<<endl;
+		cout<<"10) Insertar nuevo informe"<<endl;
+		cout<<"11) Que informe hago primero"<<endl;
+		cout<<"12) Eliminar informe"<<endl<<endl;
+		cout<<"13) Ver examenes pendientes"<<endl;
+		cout<<"14) Insertar nuevo examen"<<endl;
+		cout<<"15) Que examen estudio primero"<<endl;
+		cout<<"16) Eliminar examen"<<endl<<endl;
+		cout<<"17) Anotar fechas o datos importantes"<<endl;
 		cout<<"0) Salir"<<endl<<endl;
 		cout<<"Elije una opcion: ";
 		cin>>opcion;
@@ -200,6 +228,25 @@ int main(){
 				break;
 			}
 			case 7:{
+				for (int i = 1; i < conta; i++) {
+        			Exposicion* auxExposicion = y[i];
+        			int j = i - 1;
+        			while (j >= 0 && y[j]->mostrarMes() > auxExposicion->mostrarMes()) {
+            			y[j + 1] = y[j];
+            			j--;
+        			}
+        			y[j + 1] = auxExposicion;
+    				}
+
+    			cout << "Exposiciones ordenadas por mes:" << endl;
+    			for (int i = 0; i < conta; i++) {
+        			
+        			y[i]->mostrarTarea();
+        			cout << "---------------------------------" << endl;
+    				}
+				break;
+			}
+			case 8:{
 				if(conta==0){
 					cout<<"NO HAY EXPOSICIONES PARA ELIMINAR"<<endl;
 				}else{
@@ -217,7 +264,7 @@ int main(){
 				}
 				break;
 			}
-			case 8:{
+			case 9:{
 				if(contad==0){
 					cout<<"NO HAY INFORMES PENDIENTES...."<<endl;
 				}else{
@@ -230,7 +277,7 @@ int main(){
 				}
 				break;
 			}
-			case 9:{
+			case 10:{
 				string h;
 				string g;
 				int f;
@@ -246,10 +293,28 @@ int main(){
 				cin>>h;
 				
 				z[contad++]= new Informe(g,f,v,h);
-				
 				break;
 			}
-			case 10:{
+			case 11:{
+				for (int i = 1; i < contad; i++) {
+        			Informe* auxInforme = z[i];
+        			int j = i - 1;
+        			while (j >= 0 && z[j]->mostrarMes() > auxInforme->mostrarMes()) {
+            			z[j + 1] = z[j];
+            			j--;
+        			}
+        			z[j + 1] = auxInforme;
+    				}
+
+    			cout << "Informes ordenados por mes:" << endl;
+    			for (int i = 0; i < contad; i++) {
+        			
+        			z[i]->mostrarTarea();
+        			cout << "---------------------------------" << endl;
+    				}
+				break;
+			}
+			case 12:{
 				if(contad==0){
 					cout<<"NO HAY INFORMES PARA ELIMINAR"<<endl;
 				}else{
@@ -267,7 +332,76 @@ int main(){
 				}
 				break;
 			} 
-			case 11:{
+			case 13:{
+				if(contado==0){
+					cout<<"NO HAY EXAMENES PENDIENTES...."<<endl;
+				}else{
+					for(int i=0;i<contado;i++){
+						cout<<"Examen "<<i<<": "<<endl;
+						p[i]->mostrarTarea();
+						cout<<"---------------------------------"<<endl;
+						
+					}
+				}
+				break;
+			}
+			case 14:{
+				int o;
+				string g;
+				int f;
+				int v;
+				
+				cout<<"Ingresa el nombre del curso: ";
+				cin>>g;
+				cout<<"Ingresa el dia de la fecha de entrega: ";
+				cin>>f;
+				cout<<"Ingresa el mes de la fecha de entrega: ";
+				cin>>v;
+				cout<<"Ingresa la cantidad de preguntas del examen: ";
+				cin>>o;
+				
+				p[contado++]= new Examen(g,f,v,o);
+				
+				break;
+			}
+			case 15:{
+				for (int i = 1; i < contado; i++) {
+        			Examen* auxExamen = p[i];
+        			int j = i - 1;
+        			while (j >= 0 && p[j]->mostrarMes() > auxExamen->mostrarMes()) {
+            			p[j + 1] = p[j];
+            			j--;
+        			}
+        			p[j + 1] = auxExamen;
+    				}
+
+    			cout << "Examenes ordenados por mes:" << endl;
+    			for (int i = 0; i < contado; i++) {
+        			
+        			p[i]->mostrarTarea();
+        			cout << "---------------------------------" << endl;
+    				}
+				break;
+			}
+			case 16:{
+				if(contado==0){
+					cout<<"NO HAY EXAMENES PARA ELIMINAR"<<endl;
+				}else{
+					int num;
+					cout<<"Ingrese el indice del examen que deseas eliminar: ";
+					cin>>num;
+					if(num>=0 and num<contado){
+						delete p[num];
+						for(int i=num;i<contado-1;i++){
+							p[i]=p[i+1];
+						}
+						contado--;
+						cout<<"EXAMEN ELIMINADO EXITOSAMENTE"<<endl;
+					}
+				}
+				break;
+			}
+			case 17:{
 				ofstream archivo;
 				string apunte;
 				archivo.open("IMPORTANTE.txt",ios::app);
